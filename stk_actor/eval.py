110af0af6c3c10e602bb84781df03de1440c9794
Jeanne <jeanne.bnvt@gmail.com>
2025-12-10 00:11:44 +0100

    update eval

--- stk_actor/eval.py
+++ stk_actor/eval.py
@@ -1,13 +1,14 @@
 # eval.py
 import torch
 import gymnasium as gym
-from stable_baselines3 import TD3
+from stable_baselines3 import SAC
 from pystk2_gymnasium import AgentSpec
 from stk_actor.pystk_actor import get_wrappers, env_name, player_name
 from functools import partial
 from bbrl.agents.gymnasium import ParallelGymAgent, make_env
 from stable_baselines3.common.env_checker import check_env
-
+from pathlib import Path
+import inspect
 NUM_EPISODES = 5
 
 def main():
@@ -21,23 +22,28 @@ def main():
     )
 
     env = base_env()
-    check_env(env, warn=True)
+    print(env)
+    print(env.metadata)
+    # check_env(env, warn=True)
 
-    model_path = "./models/model.zip"
-    model = TD3.load(model_path)
+    # model_path = "./models/model"
+    mod_path = Path(inspect.getfile(get_wrappers)).parent
+
+    model = SAC.load(mod_path / "models/model.zip")
     print(env)
 
     for episode in range(NUM_EPISODES):
-        obs, info = env.reset()
+        obs = env.reset()
         done = False
         total_reward = 0.0
 
         while not done:
-            action, _states = model.predict(obs, deterministic=True)
+            with torch.no_grad():
+                action, _states = model.predict(obs, deterministic=True)
 
-            obs, reward, terminated, truncated, info = env.step(action)
-            done = terminated or truncated
-            total_reward += reward
+                obs, reward, terminated, truncated = env.step(action)
+                done = terminated or truncated
+                total_reward += reward
 
             env.render()
 
