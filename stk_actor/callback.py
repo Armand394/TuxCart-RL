8fec0709a7749e98f30bb392732dbedbf6b96053
Jeanne <jeanne.bnvt@gmail.com>
2025-12-11 14:40:39 +0100

    debug

--- stk_actor/callback.py
+++ stk_actor/callback.py
@@ -1,7 +1,7 @@
 from stable_baselines3.common.callbacks import BaseCallback
 import torch
 import numpy as np
-
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 class FeatureImportanceBarCallback(BaseCallback):
     def __init__(self, env, eval_freq=5000, verbose=1):
         super().__init__(verbose)
@@ -13,14 +13,14 @@ class FeatureImportanceBarCallback(BaseCallback):
             return True
 
         try:
-            mean_obs = self.env.get_attr("obs_rms")[0].mean
+            mean_obs = self.env.get_attr("obs_rms")[0].mean.to(device)
         except:
             mean_obs = self.env.envs[0].env.observation_space.sample()
 
-        obs = torch.tensor(mean_obs, dtype=torch.float32).unsqueeze(0)
+        obs = torch.tensor(mean_obs, dtype=torch.float32).unsqueeze(0).to(device)
         obs.requires_grad_(True)
 
-        action, _ = self.model.policy(obs, deterministic=True)
+        action, _ = self.model.policy(obs, deterministic=True).to(device)
 
         action.sum().backward()
 
